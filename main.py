3c0af1648ed4bba88bc6522ee009e6805684a95b
OmarovAK <Omarov_Anuar@mail.ru>
2022-08-09 07:16:41 +0545

    Revert "init test"
    This reverts commit fc866e955d178dd5e571e1d69f3004842db25e8a.

--- main.py
+++
@@ -1,7 +0,0 @@
-from bot import VK
-
-user_vk = VK(group_id='215217227',
-             token="vk1.a.acscNKsdVoIAwaRfAyPAEx2oq4bNmklARhMiC-zsOP8jfdJj9wFcfPYr4GuMRxbYCo4qCGwGilMy2s6kq8puKHcGTrg0ogU2DnlDrJ0ixdXgU-oGMf_asN3K_f7JpQQPJHrscsk0QbKelNZMB35RGIeCJP8VxvsEAtdjFBtO_bmElKv7oqcL8mZc_h9DPIQO",
-             ind_token="vk1.a.Rm5Lc64V6GaKQAheMOvqVyq70pDyYkIBd4eo88BToOuHg0aTx4hMmi6CnRdJwXs7S9eM1ZB3KIzu7iKy4POrxKCWjiGYaOt8ksb_5hUNgtbkf4KD6DYX5papGiRVXNeb9YEEoUd3QoEgHrExb0x_vtka64Si2JQqD7-9oBsNaO2KW5LQSnZkRMjaDfqDhf42")
-
-user_vk.vk_bot()
